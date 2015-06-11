16e3621d7520142ab2de6d1a618426f2b12e38d8
John Abraham <jef.abraham@gmail.com>
2015-06-11 11:54:43 +0530

    Changed a constant name.

--- arabic2english.rb
+++ arabic2english.rb
@@ -1,75 +1,72 @@
-# require 'awesome_print'
 
-ARR = {'crore'     => 1_00_00_000,
-       'lakhs'     => 1_00_000,
-       'thousand'  => 1_000,
-       'hundred'   => 100,
-       'one'       => 1,
-       'two'       => 2,
-       'three'     => 3,
-       'four'      => 4,
-       'five'      => 5,
-       'six'       => 6,
-       'seven'     => 7,
-       'eight'     => 8,
-       'nine'      => 9,
-       'ten'       => 10,
-       'eleven'    => 11,
-       'twelve'    => 12,
-       'thirteen'  => 13,
-       'fourteen'  => 14,
-       'fifteen'   => 15,
-       'sixteen'   => 16,
-       'seventeen' => 17,
-       'eighteen'  => 18,
-       'nineteen'  => 19,
-       'twenty'    => 20,  
-       'thirty'    => 30, 
-       'fourty'    => 40, 
-       'fifty'     => 50, 
-       'sixty'     => 60, 
-       'seventy'   => 70, 
-       'eighty'    => 80, 
-       'ninety'    => 90
-      }
+NUMBER_WORD_MAPPING = {'crore'     => 1_00_00_000,
+                       'lakhs'     => 1_00_000,
+                       'thousand'  => 1_000,
+                       'hundred'   => 100,
+                       'one'       => 1,
+                       'two'       => 2,
+                       'three'     => 3,
+                       'four'      => 4,
+                       'five'      => 5,
+                       'six'       => 6,
+                       'seven'     => 7,
+                       'eight'     => 8,
+                       'nine'      => 9,
+                       'ten'       => 10,
+                       'eleven'    => 11,
+                       'twelve'    => 12,
+                       'thirteen'  => 13,
+                       'fourteen'  => 14,
+                       'fifteen'   => 15,
+                       'sixteen'   => 16,
+                       'seventeen' => 17,
+                       'eighteen'  => 18,
+                       'nineteen'  => 19,
+                       'twenty'    => 20,  
+                       'thirty'    => 30, 
+                       'fourty'    => 40, 
+                       'fifty'     => 50, 
+                       'sixty'     => 60, 
+                       'seventy'   => 70, 
+                       'eighty'    => 80, 
+                       'ninety'    => 90
+                      }
 
 def get_word(number)
   h = {
-    "crore"     =>  (number.to_s.length == 8 || number.to_s.length == 9) && (number / 1_00_00_000) > 0,
-    "lakhs"     =>  (number.to_s.length == 6 || number.to_s.length == 7) && (number / 1_00_000) > 0,
-    "thousand"  =>  (number.to_s.length == 4 || number.to_s.length == 5) && (number / 1_000) > 0,
-    "hundred"   =>  number.to_s.length == 3 && (number / 100) > 0,
-    'one'       =>  number == 1,
-    'two'       =>  number == 2,
-    'three'     =>  number == 3,
-    'four'      =>  number == 4,
-    'five'      =>  number == 5,
-    'six'       =>  number == 6,
-    'seven'     =>  number == 7,
-    'eight'     =>  number == 8,
-    'nine'      =>  number == 9,
-    'ten'       =>  number == 10,
-    'eleven'    =>  number == 11,
-    'twelve'    =>  number == 12,
-    'thirteen'  =>  number == 13,
-    'fourteen'  =>  number == 14,
-    'fifteen'   =>  number == 15,
-    'sixteen'   =>  number == 16,
-    'seventeen' =>  number == 17,
-    'eighteen'  =>  number == 18,
-    'nineteen'  =>  number == 19,
-    'twenty'    =>  (number - (number % 10)) == 20,  
-    'thirty'    =>  (number - (number % 10)) == 30, 
-    'fourty'    =>  (number - (number % 10)) == 40, 
-    'fifty'     =>  (number - (number % 10)) == 50, 
-    'sixty'     =>  (number - (number % 10)) == 60, 
-    'seventy'   =>  (number - (number % 10)) == 70, 
-    'eighty'    =>  (number - (number % 10)) == 80, 
-    'ninety'    =>  (number - (number % 10)) == 90
+    "crore"     =>  (number.to_s.length == 8 || number.to_s.length == 9) && (number / NUMBER_WORD_MAPPING['crore']) > 0,
+    "lakhs"     =>  (number.to_s.length == 6 || number.to_s.length == 7) && (number / NUMBER_WORD_MAPPING['lakhs']) > 0,
+    "thousand"  =>  (number.to_s.length == 4 || number.to_s.length == 5) && (number / NUMBER_WORD_MAPPING['thousand']) > 0,
+    "hundred"   =>  number.to_s.length == 3 && (number / NUMBER_WORD_MAPPING['hundred']) > 0,
+    'one'       =>  number == NUMBER_WORD_MAPPING['one'],      
+    'two'       =>  number == NUMBER_WORD_MAPPING['two'],      
+    'three'     =>  number == NUMBER_WORD_MAPPING['three'],    
+    'four'      =>  number == NUMBER_WORD_MAPPING['four'],     
+    'five'      =>  number == NUMBER_WORD_MAPPING['five'],     
+    'six'       =>  number == NUMBER_WORD_MAPPING['six'],      
+    'seven'     =>  number == NUMBER_WORD_MAPPING['seven'],    
+    'eight'     =>  number == NUMBER_WORD_MAPPING['eight'],    
+    'nine'      =>  number == NUMBER_WORD_MAPPING['nine'],     
+    'ten'       =>  number == NUMBER_WORD_MAPPING['ten'],      
+    'eleven'    =>  number == NUMBER_WORD_MAPPING['eleven'],   
+    'twelve'    =>  number == NUMBER_WORD_MAPPING['twelve'],   
+    'thirteen'  =>  number == NUMBER_WORD_MAPPING['thirteen'], 
+    'fourteen'  =>  number == NUMBER_WORD_MAPPING['fourteen'], 
+    'fifteen'   =>  number == NUMBER_WORD_MAPPING['fifteen'],  
+    'sixteen'   =>  number == NUMBER_WORD_MAPPING['sixteen'],  
+    'seventeen' =>  number == NUMBER_WORD_MAPPING['seventeen'],
+    'eighteen'  =>  number == NUMBER_WORD_MAPPING['eighteen'], 
+    'nineteen'  =>  number == NUMBER_WORD_MAPPING['nineteen'], 
+    'twenty'    =>  (number - (number % 10)) == NUMBER_WORD_MAPPING['twenty'],   
+    'thirty'    =>  (number - (number % 10)) == NUMBER_WORD_MAPPING['thirty'],  
+    'fourty'    =>  (number - (number % 10)) == NUMBER_WORD_MAPPING['fourty'],  
+    'fifty'     =>  (number - (number % 10)) == NUMBER_WORD_MAPPING['fifty'],   
+    'sixty'     =>  (number - (number % 10)) == NUMBER_WORD_MAPPING['sixty'],   
+    'seventy'   =>  (number - (number % 10)) == NUMBER_WORD_MAPPING['seventy'], 
+    'eighty'    =>  (number - (number % 10)) == NUMBER_WORD_MAPPING['eighty'],  
+    'ninety'    =>  (number - (number % 10)) == NUMBER_WORD_MAPPING['ninety']
   }
 
-  # ap h
-  
   h.key(true)
 end
 
@@ -78,16 +75,14 @@ def get_full_word(number,word=[])
   
   if number >= 100
     w = get_word(number)
-    get_full_word(number / ARR[w],word)
-    #puts w
+    get_full_word(number / NUMBER_WORD_MAPPING[w],word)
     word << w
-    number = number - (number / ARR[w] * ARR[w])
+    number = number - (number / NUMBER_WORD_MAPPING[w] * NUMBER_WORD_MAPPING[w])
     get_full_word(number,word)
   else
     w = get_word(number)
-    #puts w
     word << w
-    number -= ARR[w]
+    number -= NUMBER_WORD_MAPPING[w]
     get_full_word(number,word)
   end
 
